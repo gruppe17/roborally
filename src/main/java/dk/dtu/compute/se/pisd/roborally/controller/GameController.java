99f5f7b7943e7f5aac58c23e99cdb45c75763f69
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-02-17 14:54:18 +0100

    Update GameController.java
    Deleted now unused dummy method from V1

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/GameController.java
@@ -37,24 +37,7 @@ public class GameController {
     public GameController(@NotNull Board board) {
         this.board = board;
     }
-
-    /**
-     * This is just some dummy controller operation to make a simple move to see something
-     * happening on the board. This method should eventually be deleted!
-     *
-     * @param space the space to which the current player should move
-     */
-    public void moveCurrentPlayerToSpace(@NotNull Space space)  {
-        // TODO Assignment V1: method should be implemented by the students:
-        //   - the current player should be moved to the given space
-        //     (if it is free()
-        //   - and the current player should be set to the player
-        //     following the current player
-        //   - the counter of moves in the game should be increased by one
-        //     if the player is moved
-
-    }
-
+    
     // XXX: V2
     public void startProgrammingPhase() {
         board.setPhase(Phase.PROGRAMMING);
