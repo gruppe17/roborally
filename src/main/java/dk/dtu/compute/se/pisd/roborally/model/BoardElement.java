1fab959c1c1d22b2b74cb3c40b481f6f8580c76c
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-08 15:32:49 +0100

    Added a few attributes to BoardElement

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/BoardElement.java
@@ -2,8 +2,23 @@ package dk.dtu.compute.se.pisd.roborally.model;
 
 /**
  * Represents the element on the board, e.g. walls, push panels, the priority antenna, etc.
+ * @author Rasmus Nylander, s205418@student.dtu.dk
  */
 public abstract class BoardElement {
+    public boolean passable;
+    public boolean isOpaque;
+
+    protected BoardElement(boolean passable, boolean isOpaque){
+        this.passable = passable;
+        this.isOpaque = isOpaque;
+    }
 
 
+    public boolean isPassable(){
+        return passable;
+    }
+
+    public boolean isOpaque() {
+        return isOpaque;
+    }
 }
