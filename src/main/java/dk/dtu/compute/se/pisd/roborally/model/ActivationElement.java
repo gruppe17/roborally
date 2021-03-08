7de91d5af8d8dc4096ce715321db4d647ab8573c
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-08 15:33:14 +0100

    Added an attribute and constructor to ActivationElement

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/ActivationElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/ActivationElement.java
@@ -2,7 +2,17 @@ package dk.dtu.compute.se.pisd.roborally.model;
 
 /**
  * Represents {@link BoardElement}s that can be activated in the activation phase.
+ * @author Rasmus Nylander, s205418@student.dtu.dk
  */
 public abstract class ActivationElement extends BoardElement{
+    protected Space space;
 
+    protected ActivationElement(boolean passable, boolean isOpaque, Space space){
+        super(passable, isOpaque);
+        this.space = space;
+    }
+
+    public Space getSpace() {
+        return space;
+    }
 }
