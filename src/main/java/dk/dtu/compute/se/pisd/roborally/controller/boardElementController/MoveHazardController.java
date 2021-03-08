5c6217ccd455867d1455f8478afa3ad402aa0ebd
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-08 15:41:25 +0100

    Initial implementation of MoveHazard class

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/boardElementController/MoveHazardController.java
@@ -0,0 +1,23 @@
+package dk.dtu.compute.se.pisd.roborally.controller.boardElementController;
+
+import dk.dtu.compute.se.pisd.roborally.model.MoveHazard;
+import dk.dtu.compute.se.pisd.roborally.model.Player;
+
+/**
+ * <p>Represents a controller for a {@link MoveHazard}.</p>
+ * @author Rasmus Nylander, s205418@student.dtu.dk
+ */
+public class MoveHazardController implements IBoardElementController {
+    private MoveHazard model;
+    public MoveHazardController(MoveHazard model){
+        this.model = model;
+    }
+
+    @Override
+    public void activate() {
+        Player player = model.getSpace().getPlayer();
+        if (player == null) return;
+        player.playerController.move(model.direction, model.distance);
+        player.playerController.turnRight(model.rotation);
+    }
+}
