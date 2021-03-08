c8806748558899328d243e39c70025e0fe534c46
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-08 15:38:27 +0100

    Added interface IBoardElementController

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/boardElementController/IBoardElementController.java
@@ -0,0 +1,10 @@
+package dk.dtu.compute.se.pisd.roborally.controller.boardElementController;
+
+
+/**
+ * This interface defines how a board element controller, such as a {@link MoveHazardController}, should look.
+ * @author Rasmus Nylander, s205418@student.dtu.dk
+ */
+public interface IBoardElementController {
+    public void activate();
+}
