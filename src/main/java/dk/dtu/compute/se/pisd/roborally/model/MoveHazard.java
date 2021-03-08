0e565dce1d97f7e1d2d082d38bf0ea72e6888cec
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-08 15:41:01 +0100

    Basic implementation of MoveHazard class

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/MoveHazard.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/MoveHazard.java
@@ -1,4 +1,28 @@
 package dk.dtu.compute.se.pisd.roborally.model;
 
+import javafx.scene.shape.MoveTo;
+
+/**
+ * <p>Represents a {@link ActivationElement} that moves the player.
+ * E.g. push panels and conveyor belts.</p>
+ * @author Rasmus Nylander, s205418@student.dtu.dk
+ */
 public class MoveHazard extends ActivationElement {
+    /**
+     * <p>The direction this {@link MoveHazard} moves a {@link Player} in. May be null.</p>
+     */
+    public Heading direction;
+    /**
+     * <p>The number of clockwise turns. May be negative.</p>
+     */
+    public int rotation;
+    /**
+     * <p>The distance to move a {@link Player}.</p>
+     */
+    public int distance;
+
+    protected MoveHazard(boolean passable, boolean isOpaque, Space space){
+        super(passable, isOpaque, space);
+    }
+
 }
