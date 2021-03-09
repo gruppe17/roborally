a4714cc1ed222e112975f50177fae4827b984b86
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-03-09 14:55:54 +0100

    Added Laser interface with a fire method.

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/interfaces/ILaser.java
@@ -0,0 +1,10 @@
+package dk.dtu.compute.se.pisd.roborally.interfaces;
+
+import dk.dtu.compute.se.pisd.roborally.model.Board;
+
+public interface ILaser {
+    /**
+     * <p>Fires a laser according to it's implementation.</p>
+     */
+    void fire();
+}
