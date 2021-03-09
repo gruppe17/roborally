37b1844b66c6454e73418f5536945a874f5c3d81
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-03-09 14:56:34 +0100

    Started work on BoardLaser

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElement/BoardLaser.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElement/BoardLaser.java
@@ -1,6 +1,14 @@
 package dk.dtu.compute.se.pisd.roborally.model.boardElement;
 
-public class BoardLaser {
+import dk.dtu.compute.se.pisd.roborally.interfaces.ILaser;
+
+public class BoardLaser implements ILaser {
+
+    @Override
+    public void fire() {
+
+    }
+
     /**
      * <p>Represents the start and end points of the laser.</p>
      * <p>{start x, start y, end x, end y}</p>
