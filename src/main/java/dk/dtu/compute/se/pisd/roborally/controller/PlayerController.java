a5f9918ea0e029af5c988ebb84bad74ac548a407
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-03-10 14:39:23 +0100

    Added Fire method again after merging from A3

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PlayerController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/PlayerController.java
@@ -1,8 +1,9 @@
 package dk.dtu.compute.se.pisd.roborally.controller;
 
+import dk.dtu.compute.se.pisd.roborally.interfaces.ILaser;
 import dk.dtu.compute.se.pisd.roborally.model.*;
 
-public class PlayerController {
+public class PlayerController implements ILaser {
     Player player;
 
     public PlayerController(Player player) {
@@ -134,6 +135,64 @@ public class PlayerController {
         CommandCardField emptyCardField = player.getEmptyCardField();
         if(emptyCardField != null){
             emptyCardField.setCard(generateRandomCommandCard());
+        }
+
+    }
+
+    /**
+     * <p>Fire in the direction the owner
+     * robot is facing. Their range has no
+     * limit. Any robot in the line of sight is
+     * shot. Robot lasers cannot fire through
+     * walls or shoot more than one robot.</p>
+     * @author Tobias Maneschijn, s205422@student.dtu.dk
+     */
+    @Override
+    public void fire() {
+        Space currentSpace = player.getSpace();
+        Space lastSpace = currentSpace;
+
+        /* Abort if player is not in a space*/
+        if (lastSpace == null)
+            return;
+
+        while (true) {
+            /* get next space */
+            lastSpace = player.board.getNeighbour(lastSpace, player.getHeading());
+            Player playerAtSpace = lastSpace.getPlayer();
+            /* remember to add the right elements to prevent hitting walls and stuff here */
+           /* if(lastSpace.element == WallBoardElement || lastSpace.element == PriorityAntennaBoardElement){
+                break;
+            }*/
+
+            /* exit if nothing is hit */
+            if (lastSpace == null) {
+                break;
+            }
+
+            /* We should add fx to the spaces that are hit*/
+
+            /* If player is hit, then damage it and do stuff. */
+            if (playerAtSpace != null && playerAtSpace != player) {
+
+                // hit player should take a SPAM damage card.
+
+                break;
+            }
+        }
+
+        /* Reset last space*/
+        currentSpace = player.getSpace();
+        lastSpace = currentSpace;
+
+
+        /*Maybe do some cleanup of fx here?*/
+        while (true) {
+            /* get next space */
+            lastSpace = player.board.getNeighbour(lastSpace, player.getHeading());
+            if (lastSpace == null) {
+                break;
+            }
         }
 
     }
