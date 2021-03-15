09177af2a2846715a1b322384357eeb8b9002abe
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-15 11:38:08 +0100

    ActivationElement now has a priority attribute

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElement/ActivationElement.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/boardElement/ActivationElement.java
@@ -7,14 +7,24 @@ import dk.dtu.compute.se.pisd.roborally.model.Space;
  * @author Rasmus Nylander, s205418@student.dtu.dk
  */
 public abstract class ActivationElement extends BoardElement{
+    /**
+     * <p>The space this {@link ActivationElement} is occupying</p>
+     */
     protected Space space;
+    /**
+     * <p>The activation priority of the element.</p>
+     */
+    protected int priority;
 
-    protected ActivationElement(boolean passable, boolean isOpaque, Space space){
+    protected ActivationElement(boolean passable, boolean isOpaque, Space space, int priority){
         super(passable, isOpaque);
         this.space = space;
+        this.priority = priority;
     }
 
     public Space getSpace() {
         return space;
     }
+
+    public int getPriority() {return priority;}
 }
