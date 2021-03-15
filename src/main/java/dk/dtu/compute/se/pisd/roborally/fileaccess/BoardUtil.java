0685dc9943aa10b6c9454ecc73b401d4ba489e15
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-15 15:36:38 +0100

    Added class BoardUtil

---
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/BoardUtil.java
@@ -0,0 +1,23 @@
+package dk.dtu.compute.se.pisd.roborally.fileaccess;
+
+import dk.dtu.compute.se.pisd.roborally.model.board.Board;
+
+public class BoardUtil {
+    private static final String BOARDSFOLDER = "boards";
+    private static final String DEFAULTBOARD = "defaultboard";
+    private static final String JSON_EXT = "json";
+
+    public static Board loadBoardFromFile(String boardName){
+        if (boardName == null){
+            assert false;
+            throw new
+        }
+
+    }
+
+    public static void saveBoardToFile(){
+        
+    }
+
+
+}
