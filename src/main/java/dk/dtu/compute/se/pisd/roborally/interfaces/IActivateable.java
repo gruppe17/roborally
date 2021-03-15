ea73d59b0e87132ee282825f81e006886d82659d
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-15 13:01:58 +0100

    javadoc

--- src/main/java/dk/dtu/compute/se/pisd/roborally/interfaces/IActivateable.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/interfaces/IActivateable.java
@@ -1,5 +1,10 @@
 package dk.dtu.compute.se.pisd.roborally.interfaces;
 
+import dk.dtu.compute.se.pisd.roborally.model.boardElement.activationElements.ActivationElement;
+
+/**
+ * <p>Represents something that can be activated, e.g. an {@link ActivationElement} or a robot laser.</p>
+ */
 public interface IActivateable{
 
     public void activate();
