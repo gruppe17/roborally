6c5578873ca0aa9725e827cc46c70d673df4d2ce
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-22 15:22:38 +0100

    Added database details

--- src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/dal/Connector.java
@@ -37,10 +37,10 @@ import java.sql.Statement;
  */
 class Connector {
     //private static final String HOST     = "localhost";
-	private static final String HOST     = "tobiasmaneschijn.com";
+	private static final String HOST     = "104.223.62.200";
     private static final int    PORT     = 3306;
-    private static final String DATABASE = "VPROBORALLY";
-    private static final String USERNAME = "user";
+    private static final String DATABASE = "tobiasma_roborally";
+    private static final String USERNAME = "tobiasma_robouser";
     private static final String PASSWORD = "";
 
     private static final String DELIMITER = ";;";
