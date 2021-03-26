58bf97dfdcb29bb1b9176c4b06045078e2236fc8
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-03-26 16:40:33 +0100

    Added semicolons to createschema.sql

--- src/main/resources/schemas/createschema.sql
+++ src/main/resources/schemas/createschema.sql
@@ -2,63 +2,63 @@
 SET FOREIGN_KEY_CHECKS = 0;;
 
 CREATE TABLE IF NOT EXISTS Game (
-  gameID int NOT NULL UNIQUE AUTO_INCREMENT,
-  
-  name varchar(255),
+                                    gameID int NOT NULL UNIQUE AUTO_INCREMENT,
 
-  phase tinyint,
-  step tinyint,
-  currentPlayer tinyint NULL,
-  
-  PRIMARY KEY (gameID),
-  FOREIGN KEY (gameID, currentPlayer) REFERENCES Player(gameID, playerID)
-);;
-  
+                                    name varchar(255),
+
+    phase tinyint,
+    step tinyint,
+    currentPlayer tinyint NULL,
+
+    PRIMARY KEY (gameID),
+    FOREIGN KEY (gameID, currentPlayer) REFERENCES Player(gameID, playerID)
+    );;
+
 CREATE TABLE IF NOT EXISTS Player (
-  gameID int NOT NULL,
-  playerID tinyint NOT NULL,
+                                      gameID int NOT NULL,
+                                      playerID tinyint NOT NULL,
 
-  name varchar(255),
-  colour varchar(31),
-  
-  positionX int,
-  positionY int,
-  heading tinyint,
-  
-  PRIMARY KEY (gameID, playerID),
-  FOREIGN KEY (gameID) REFERENCES Game(gameID) ON DELETE CASCADE
-);;
+                                      name varchar(255),
+    colour varchar(31),
+
+    positionX int,
+    positionY int,
+    heading tinyint,
+
+    PRIMARY KEY (gameID, playerID),
+    FOREIGN KEY (gameID) REFERENCES Game(gameID) ON DELETE CASCADE
+    );;
 
 CREATE TABLE IF NOT EXISTS ActivationQueue (
-    gameID int NOT NULL,
-    playerID tinyint NOT NULL,
-    priority tinyint NOT NULL,
+                                               gameID int NOT NULL,
+                                               playerID tinyint NOT NULL,
+                                               priority tinyint NOT NULL,
 
-    PRIMARY KEY (gameID, priority),
+                                               PRIMARY KEY (gameID, priority),
     FOREIGN KEY (gameID) REFERENCES Game(gameID) ON DELETE CASCADE,
     FOREIGN KEY (gameID, playerID) REFERENCES Player(gameID, playerID) ON DELETE CASCADE
-);;
+    );;
 
 CREATE TABLE IF NOT EXISTS Card (
-    cardID int NOT NULL UNIQUE,
-    gameID int NOT NULL,
-    playerID tinyint NOT NULL,
-    type tinyint NOT NULL,
-    position tinyint NOT NULL,
+                                    cardID int NOT NULL UNIQUE,
+                                    gameID int NOT NULL,
+                                    playerID tinyint NOT NULL,
+                                    type tinyint NOT NULL,
+                                    position tinyint NOT NULL,
 
-    PRIMARY KEY (cardID),
+                                    PRIMARY KEY (cardID),
     FOREIGN KEY (gameID) REFERENCES Game(gameID) ON DELETE CASCADE,
     FOREIGN KEY (gameID, playerID) REFERENCES Player(gameID, playerID) ON DELETE CASCADE
-);;
+    );;
 
 CREATE TABLE IF NOT EXISTS CardCommand(
-    cardID int NOT NULL,
-    command tinyint NOT NULL,
+                                          cardID int NOT NULL,
+                                          command tinyint NOT NULL,
 
-    PRIMARY KEY (cardID, command),
+                                          PRIMARY KEY (cardID, command),
     FOREIGN KEY (cardID) REFERENCES Card(cardID) ON DELETE CASCADE
-)
+    );;
 
-SET FOREIGN_KEY_CHECKS = 1;;
+    SET FOREIGN_KEY_CHECKS = 1;;
 
 // TODO still some stuff missing here
