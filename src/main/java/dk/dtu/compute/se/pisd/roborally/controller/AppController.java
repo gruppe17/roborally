192ddfd77b18a9629b126fa50e340aae4616d860
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-03-27 14:15:41 +0100

    Whitespace

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/AppController.java
@@ -50,123 +50,123 @@ import java.util.Optional;
  */
 public class AppController implements Observer {
 
-    final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
-    final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
+	final private List<Integer> PLAYER_NUMBER_OPTIONS = Arrays.asList(2, 3, 4, 5, 6);
+	final private List<String> PLAYER_COLORS = Arrays.asList("red", "green", "blue", "orange", "grey", "magenta");
 
-    final private RoboRally roboRally;
+	final private RoboRally roboRally;
 
-    private GameController gameController;
+	private GameController gameController;
 
-    public AppController(@NotNull RoboRally roboRally) {
-        this.roboRally = roboRally;
-    }
+	public AppController(@NotNull RoboRally roboRally) {
+		this.roboRally = roboRally;
+	}
 
-    public void newGame() {
-        ChoiceDialog<Integer> dialog = new ChoiceDialog<>(PLAYER_NUMBER_OPTIONS.get(0), PLAYER_NUMBER_OPTIONS);
-        dialog.setTitle("Player number");
-        dialog.setHeaderText("Select number of players");
-        Optional<Integer> result = dialog.showAndWait();
+	public void newGame() {
+		ChoiceDialog<Integer> dialog = new ChoiceDialog<>(PLAYER_NUMBER_OPTIONS.get(0), PLAYER_NUMBER_OPTIONS);
+		dialog.setTitle("Player number");
+		dialog.setHeaderText("Select number of players");
+		Optional<Integer> result = dialog.showAndWait();
 
-        if (!result.isPresent()) return;
+		if (!result.isPresent()) return;
 
-        // The UI should not allow this, but in case this happens anyway.
-        // give the user the option to save the game or abort this operation!
-        if (gameController != null && !stopGame()) return;
+		// The UI should not allow this, but in case this happens anyway.
+		// give the user the option to save the game or abort this operation!
+		if (gameController != null && !stopGame()) return;
 
-        // XXX the board should eventually be created programmatically or loaded from a file
-        //     here we just create an empty board with the required number of players.
-        Board board = BoardLoader.loadBoard("defaultboard");
-        Game game = new Game(board);
-        gameController = new GameController(game);
-        int no = result.get();
-        for (int i = 0; i < no; i++) {
-            Player player = new Player(game, PLAYER_COLORS.get(i), "Player " + (i + 1));
-            game.addPlayer(player);
-            player.setSpace(board.getSpace(i % board.width, i));
-        }
-
-
-        // XXX: V2
-        // board.setCurrentPlayer(board.getPlayer(0));
-        gameController.startProgrammingPhase();
-
-        roboRally.createGameView(gameController);
-
-    }
-
-    /**
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    public void saveGame() {
-        //Todo: Name the save
-        //Todo: Determine whether to create or update
-        // If possible to update maybe ask player whether to overwrite the old save
-        if (gameController == null) return;
-        RepositoryAccess.getRepository().createGameInDB(gameController.game);
-
-    }
-
-    /**
-     *
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    public void loadGame() {
-        // XXX needs to be implememted eventually
-        // for now, we just create a new game
-        if (gameController != null) return;
-        GameInDB[] gameInDBs = RepositoryAccess.getRepository().getGames().toArray(new GameInDB[0]);
-        //Todo: show user names and get their choice.
-        gameController = new GameController(RepositoryAccess.getRepository().loadGameFromDB(gameInDBs[0].id));
-        roboRally.createGameView(gameController);
-    }
-
-    /**
-     * Stop playing the current game, giving the user the option to save
-     * the game or to cancel stopping the game. The method returns true
-     * if the game was successfully stopped (with or without saving the
-     * game); returns false, if the current game was not stopped. In case
-     * there is no current game, false is returned.
-     *
-     * @return true if the current game was stopped, false otherwise
-     */
-    public boolean stopGame() {
-        if (gameController == null) return false;
-
-        // here we save the game (without asking the user).
-        saveGame();
-
-        gameController = null;
-        roboRally.createGameView(null);
-        return true;
-    }
-
-    public void exit() {
-        if (gameController != null) {
-            Alert alert = new Alert(AlertType.CONFIRMATION);
-            alert.setTitle("Exit RoboRally?");
-            alert.setContentText("Are you sure you want to exit RoboRally?");
-            Optional<ButtonType> result = alert.showAndWait();
-
-            if (!result.isPresent() || result.get() != ButtonType.OK) {
-                return; // return without exiting the application
-            }
-        }
-
-        // If the user did not cancel, the RoboRally application will exit
-        // after the option to save the game
-        if (gameController == null || stopGame()) {
-            Platform.exit();
-        }
-    }
-
-    public boolean isGameRunning() {
-        return gameController != null;
-    }
+		// XXX the board should eventually be created programmatically or loaded from a file
+		//     here we just create an empty board with the required number of players.
+		Board board = BoardLoader.loadBoard("defaultboard");
+		Game game = new Game(board);
+		gameController = new GameController(game);
+		int no = result.get();
+		for (int i = 0; i < no; i++) {
+			Player player = new Player(game, PLAYER_COLORS.get(i), "Player " + (i + 1));
+			game.addPlayer(player);
+			player.setSpace(board.getSpace(i % board.width, i));
+		}
 
 
-    @Override
-    public void update(Subject subject) {
-        // XXX do nothing for now
-    }
+		// XXX: V2
+		// board.setCurrentPlayer(board.getPlayer(0));
+		gameController.startProgrammingPhase();
+
+		roboRally.createGameView(gameController);
+
+	}
+
+	/**
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	public void saveGame() {
+		//Todo: Name the save
+		//Todo: Determine whether to create or update
+		// If possible to update maybe ask player whether to overwrite the old save
+		if (gameController == null) return;
+		RepositoryAccess.getRepository().createGameInDB(gameController.game);
+
+	}
+
+	/**
+	 *
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	public void loadGame() {
+		// XXX needs to be implememted eventually
+		// for now, we just create a new game
+		if (gameController != null) return;
+		GameInDB[] gameInDBs = RepositoryAccess.getRepository().getGames().toArray(new GameInDB[0]);
+		//Todo: show user names and get their choice.
+		gameController = new GameController(RepositoryAccess.getRepository().loadGameFromDB(gameInDBs[0].id));
+		roboRally.createGameView(gameController);
+	}
+
+	/**
+	 * Stop playing the current game, giving the user the option to save
+	 * the game or to cancel stopping the game. The method returns true
+	 * if the game was successfully stopped (with or without saving the
+	 * game); returns false, if the current game was not stopped. In case
+	 * there is no current game, false is returned.
+	 *
+	 * @return true if the current game was stopped, false otherwise
+	 */
+	public boolean stopGame() {
+		if (gameController == null) return false;
+
+		// here we save the game (without asking the user).
+		saveGame();
+
+		gameController = null;
+		roboRally.createGameView(null);
+		return true;
+	}
+
+	public void exit() {
+		if (gameController != null) {
+			Alert alert = new Alert(AlertType.CONFIRMATION);
+			alert.setTitle("Exit RoboRally?");
+			alert.setContentText("Are you sure you want to exit RoboRally?");
+			Optional<ButtonType> result = alert.showAndWait();
+
+			if (!result.isPresent() || result.get() != ButtonType.OK) {
+				return; // return without exiting the application
+			}
+		}
+
+		// If the user did not cancel, the RoboRally application will exit
+		// after the option to save the game
+		if (gameController == null || stopGame()) {
+			Platform.exit();
+		}
+	}
+
+	public boolean isGameRunning() {
+		return gameController != null;
+	}
+
+
+	@Override
+	public void update(Subject subject) {
+		// XXX do nothing for now
+	}
 
 }
--- src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/BoardLoader.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/fileaccess/BoardLoader.java
@@ -44,199 +44,199 @@ import java.util.Arrays;
  */
 public class BoardLoader {
 
-    private static final String BOARDSFOLDER = "boards";
-    private static final String DEFAULTBOARD = "defaultboard";
-    private static final String JSON_EXT = "json";
+	private static final String BOARDSFOLDER = "boards";
+	private static final String DEFAULTBOARD = "defaultboard";
+	private static final String JSON_EXT = "json";
 
-    /**
-     * <p>Loads a {@link Board} from a file and returns it.</p>
-     *
-     * @param boardName the name of the board which is to be loaded
-     * @return a new board that was generated from a file
-     * @throws IOException
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    public static Board loadBoard(String boardName) {
-        if (boardName == null) {
-            boardName = DEFAULTBOARD;
-        }
+	/**
+	 * <p>Loads a {@link Board} from a file and returns it.</p>
+	 *
+	 * @param boardName the name of the board which is to be loaded
+	 * @return a new board that was generated from a file
+	 * @throws IOException
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	public static Board loadBoard(String boardName) {
+		if (boardName == null) {
+			boardName = DEFAULTBOARD;
+		}
 
-        ClassLoader classLoader = BoardLoader.class.getClassLoader();
-        InputStream inputStream = classLoader.getResourceAsStream(BOARDSFOLDER + "/" + boardName + "." + JSON_EXT);
-        if (inputStream == null) {
-            // TODO these constants should be defined somewhere
-            return new Board(8, 8);
-        }
-
-
-        // In simple cases, we can create a Gson object with: new Gson()
-        GsonBuilder simpleBuilder = new GsonBuilder().registerTypeAdapter(
-                BoardElement.class, new Adapter<BoardElement>());
-        //new GsonBuilder().registerTypeAdapter(FieldAction.class, new Adapter<FieldAction>());
-        Gson gson = simpleBuilder.create();
+		ClassLoader classLoader = BoardLoader.class.getClassLoader();
+		InputStream inputStream = classLoader.getResourceAsStream(BOARDSFOLDER + "/" + boardName + "." + JSON_EXT);
+		if (inputStream == null) {
+			// TODO these constants should be defined somewhere
+			return new Board(8, 8);
+		}
 
 
-        Board result;
-        // FileReader fileReader = null;
-        JsonReader reader = null;
-        try {
-            // fileReader = new FileReader(filename);
-            reader = gson.newJsonReader(new InputStreamReader(inputStream));
-            BoardTemplate template = gson.fromJson(reader, BoardTemplate.class);
-
-            result = boardFromBoardTemplate(template);
-            reader.close();
-            return result;
-        } catch (IOException e1) {
-            if (reader != null) {
-                try {
-                    reader.close();
-                    inputStream = null;
-                } catch (IOException e2) {
-                }
-            }
-            if (inputStream != null) {
-                try {
-                    inputStream.close();
-                } catch (IOException e2) {
-                }
-            }
-        }
-        return null;
-    }
+		// In simple cases, we can create a Gson object with: new Gson()
+		GsonBuilder simpleBuilder = new GsonBuilder().registerTypeAdapter(
+				BoardElement.class, new Adapter<BoardElement>());
+		//new GsonBuilder().registerTypeAdapter(FieldAction.class, new Adapter<FieldAction>());
+		Gson gson = simpleBuilder.create();
 
 
-    /**
-     * <p>Saves a {@link Board} to as a jason file.</p>
-     *
-     * @param board The board to save
-     * @param name  the name of the file that should be written to
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    public static void saveBoard(@NotNull Board board, String name) {
-        BoardTemplate template = boardToBoardTemplate(board, name);
+		Board result;
+		// FileReader fileReader = null;
+		JsonReader reader = null;
+		try {
+			// fileReader = new FileReader(filename);
+			reader = gson.newJsonReader(new InputStreamReader(inputStream));
+			BoardTemplate template = gson.fromJson(reader, BoardTemplate.class);
 
-        ClassLoader classLoader = BoardLoader.class.getClassLoader();
-        // TODO: this is not very defensive, and will result in a NullPointerException
-        //       when the folder "resources" does not exist! But, it does not need
-        //       the file "simpleCards.json" to exist!
-        String filename = classLoader.getResource(BOARDSFOLDER).getPath() + "/" + name + "." + JSON_EXT;
+			result = boardFromBoardTemplate(template);
+			reader.close();
+			return result;
+		} catch (IOException e1) {
+			if (reader != null) {
+				try {
+					reader.close();
+					inputStream = null;
+				} catch (IOException e2) {
+				}
+			}
+			if (inputStream != null) {
+				try {
+					inputStream.close();
+				} catch (IOException e2) {
+				}
+			}
+		}
+		return null;
+	}
 
-        // In simple cases, we can create a Gson object with new:
-        //
-        //   Gson gson = new Gson();
-        //
-        // But, if you need to configure it, it is better to create it from
-        // a builder (here, we want to configure the JSON serialisation with
-        // a pretty printer):
-        GsonBuilder simpleBuilder = new GsonBuilder().
-                registerTypeAdapter(BoardElement.class, new Adapter<BoardElement>()).
-                setPrettyPrinting();
-        Gson gson = simpleBuilder.create();
 
-        FileWriter fileWriter = null;
-        JsonWriter writer = null;
-        try {
-            fileWriter = new FileWriter(filename);
-            writer = gson.newJsonWriter(fileWriter);
-            gson.toJson(template, template.getClass(), writer);
-        } catch (IOException e1) {
-            assert false;
-        } finally {
-            if (writer != null) {
-                try {
-                    writer.close();
-                } catch (IOException e2) {
-                }
-            }
-            if (fileWriter != null) {
-                try {
-                    fileWriter.close();
-                } catch (IOException e2) {
-                }
-            }
-            for (int i = 0; i < board.width; i++) {
-                for (int j = 0; j < board.height; j++) {
-                    Space space = board.getSpace(i, j);
-                    if (space == null) continue;
-                    for (ActivationElement boardElement: space.getActivationElements()) {
-                        boardElement.setSpace(space);
-                    }
-                }
-            }
-        }
-    }
+	/**
+	 * <p>Saves a {@link Board} to as a jason file.</p>
+	 *
+	 * @param board The board to save
+	 * @param name  the name of the file that should be written to
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	public static void saveBoard(@NotNull Board board, String name) {
+		BoardTemplate template = boardToBoardTemplate(board, name);
 
-    /**
-     * <p>Creates a new {@link Board} from a {@link BoardTemplate}.</p>
-     *
-     * @param template the template from which to create a new board
-     * @return a new board created from the template
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    @NotNull
-    private static Board boardFromBoardTemplate(BoardTemplate template) {
-        Board board = new Board(template.width, template.height, template.name);
-        for (SpaceTemplate spaceTemplate : template.spaces) {
-            Space space = board.getSpace(spaceTemplate.x, spaceTemplate.y);
-            if (space == null) continue;
-            //todo add all method in space?
-            for (BoardElement boardElement : spaceTemplate.boardElements) {
-                space.addBoardElement(boardElement);
-                if (boardElement instanceof ActivationElement) ((ActivationElement) boardElement).setSpace(space);
-            }
+		ClassLoader classLoader = BoardLoader.class.getClassLoader();
+		// TODO: this is not very defensive, and will result in a NullPointerException
+		//       when the folder "resources" does not exist! But, it does not need
+		//       the file "simpleCards.json" to exist!
+		String filename = classLoader.getResource(BOARDSFOLDER).getPath() + "/" + name + "." + JSON_EXT;
 
-        }
-        return board;
-    }
+		// In simple cases, we can create a Gson object with new:
+		//
+		//   Gson gson = new Gson();
+		//
+		// But, if you need to configure it, it is better to create it from
+		// a builder (here, we want to configure the JSON serialisation with
+		// a pretty printer):
+		GsonBuilder simpleBuilder = new GsonBuilder().
+				registerTypeAdapter(BoardElement.class, new Adapter<BoardElement>()).
+				setPrettyPrinting();
+		Gson gson = simpleBuilder.create();
 
-    /**
-     * <p>Creates a new {@link BoardTemplate} from a {@link Board}.</p>
-     *
-     * @param board the board of which to create a template
-     * @return a new board template of the specified board
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    @NotNull
-    private static BoardTemplate boardToBoardTemplate(@NotNull Board board, String name) {
-        BoardTemplate template = new BoardTemplate();
-        template.name = name;
-        template.width = board.width;
-        template.height = board.height;
+		FileWriter fileWriter = null;
+		JsonWriter writer = null;
+		try {
+			fileWriter = new FileWriter(filename);
+			writer = gson.newJsonWriter(fileWriter);
+			gson.toJson(template, template.getClass(), writer);
+		} catch (IOException e1) {
+			assert false;
+		} finally {
+			if (writer != null) {
+				try {
+					writer.close();
+				} catch (IOException e2) {
+				}
+			}
+			if (fileWriter != null) {
+				try {
+					fileWriter.close();
+				} catch (IOException e2) {
+				}
+			}
+			for (int i = 0; i < board.width; i++) {
+				for (int j = 0; j < board.height; j++) {
+					Space space = board.getSpace(i, j);
+					if (space == null) continue;
+					for (ActivationElement boardElement: space.getActivationElements()) {
+						boardElement.setSpace(space);
+					}
+				}
+			}
+		}
+	}
 
-        if (name == null) template.name = board.getBoardName();
+	/**
+	 * <p>Creates a new {@link Board} from a {@link BoardTemplate}.</p>
+	 *
+	 * @param template the template from which to create a new board
+	 * @return a new board created from the template
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	@NotNull
+	private static Board boardFromBoardTemplate(BoardTemplate template) {
+		Board board = new Board(template.width, template.height, template.name);
+		for (SpaceTemplate spaceTemplate : template.spaces) {
+			Space space = board.getSpace(spaceTemplate.x, spaceTemplate.y);
+			if (space == null) continue;
+			//todo add all method in space?
+			for (BoardElement boardElement : spaceTemplate.boardElements) {
+				space.addBoardElement(boardElement);
+				if (boardElement instanceof ActivationElement) ((ActivationElement) boardElement).setSpace(space);
+			}
 
-        for (int i = 0; i < board.width; i++) {
-            for (int j = 0; j < board.height; j++) {
-                Space space = board.getSpace(i, j);
-                if (space.getElements().length < 1) continue;
-                template.spaces.add(spaceTemplateFromSpace(space));
-            }
-        }
-        return template;
-    }
+		}
+		return board;
+	}
 
-    /**
-     * <p>Creates a new {@link SpaceTemplate} of a specified {@link Space}.</p>
-     *
-     * @param space the space of which to create a template
-     * @return a new SpaceTemplate of a specified space
-     * @author Rasmus Nylander, s205418@student.dtu.dk
-     */
-    private static SpaceTemplate spaceTemplateFromSpace(@NotNull Space space) {
-        SpaceTemplate spaceTemplate = new SpaceTemplate();
-        spaceTemplate.x = space.x;
-        spaceTemplate.y = space.y;
+	/**
+	 * <p>Creates a new {@link BoardTemplate} from a {@link Board}.</p>
+	 *
+	 * @param board the board of which to create a template
+	 * @return a new board template of the specified board
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	@NotNull
+	private static BoardTemplate boardToBoardTemplate(@NotNull Board board, String name) {
+		BoardTemplate template = new BoardTemplate();
+		template.name = name;
+		template.width = board.width;
+		template.height = board.height;
+
+		if (name == null) template.name = board.getBoardName();
+
+		for (int i = 0; i < board.width; i++) {
+			for (int j = 0; j < board.height; j++) {
+				Space space = board.getSpace(i, j);
+				if (space.getElements().length < 1) continue;
+				template.spaces.add(spaceTemplateFromSpace(space));
+			}
+		}
+		return template;
+	}
+
+	/**
+	 * <p>Creates a new {@link SpaceTemplate} of a specified {@link Space}.</p>
+	 *
+	 * @param space the space of which to create a template
+	 * @return a new SpaceTemplate of a specified space
+	 * @author Rasmus Nylander, s205418@student.dtu.dk
+	 */
+	private static SpaceTemplate spaceTemplateFromSpace(@NotNull Space space) {
+		SpaceTemplate spaceTemplate = new SpaceTemplate();
+		spaceTemplate.x = space.x;
+		spaceTemplate.y = space.y;
         /*
         for (BoardElement boardElement: space.getElements()) {
             spaceTemplate.boardElements.add(boardElementToBoardElementTemplate(boardElement));
         }
          */
-        spaceTemplate.boardElements.addAll(Arrays.asList(space.getElements()));
-        for (ActivationElement boardElement: space.getActivationElements()) {
-            boardElement.setSpace(null);
-        }
-        return spaceTemplate;
-    }
+		spaceTemplate.boardElements.addAll(Arrays.asList(space.getElements()));
+		for (ActivationElement boardElement: space.getActivationElements()) {
+			boardElement.setSpace(null);
+		}
+		return spaceTemplate;
+	}
 
 }
