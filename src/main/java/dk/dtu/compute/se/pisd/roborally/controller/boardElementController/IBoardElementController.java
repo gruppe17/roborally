97253e95f5582cfc0f788f37558894b4c3ea7dcb
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-04-08 17:00:28 +0200

    IBoardElementController now requires getBoardElement method

--- src/main/java/dk/dtu/compute/se/pisd/roborally/controller/boardElementController/IBoardElementController.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/controller/boardElementController/IBoardElementController.java
@@ -2,11 +2,13 @@ package dk.dtu.compute.se.pisd.roborally.controller.boardElementController;
 
 
 import dk.dtu.compute.se.pisd.roborally.interfaces.IActivateable;
+import dk.dtu.compute.se.pisd.roborally.model.board.boardElement.activationElements.ActivationElement;
+import org.jetbrains.annotations.NotNull;
 
 /**
  * This interface defines how a board element controller, such as a {@link MoveHazardController}, should look.
  * @author Rasmus Nylander, s205418@student.dtu.dk
  */
 public interface IBoardElementController extends IActivateable {
-
+	public @NotNull ActivationElement getBoardElement();
 }
