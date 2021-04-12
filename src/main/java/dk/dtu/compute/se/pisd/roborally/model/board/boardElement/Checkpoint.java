2ea3ee9d06cc0bb329ef8379c702888c0e9cfebd
Tobias Nyholm Maneschijn <tfrom222@gmail.com>
2021-04-12 15:43:08 +0200

    Added checkpoint number to Checkpoint

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/board/boardElement/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/board/boardElement/Checkpoint.java
@@ -1,4 +1,18 @@
 package dk.dtu.compute.se.pisd.roborally.model.board.boardElement;
 
-public class Checkpoint {
+import dk.dtu.compute.se.pisd.roborally.model.enums.Heading;
+import org.jetbrains.annotations.NotNull;
+
+public class Checkpoint extends BoardElement{
+
+    private int number = 1;
+
+    public int getNumber() {
+        return number;
+    }
+
+    public Checkpoint(@NotNull Heading[] position, @NotNull Heading[] impassableFrom, @NotNull Heading[] opaqueFrom, Heading direction) {
+        super(position, impassableFrom, opaqueFrom, direction);
+
+    }
 }
