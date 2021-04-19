289df9aca618b051c0aecb6a51b05a396a37dd12
RasmusNylander <43042651+RasmusNylander@users.noreply.github.com>
2021-04-19 16:59:51 +0200

    Checkpoint now passed number in constructor

--- src/main/java/dk/dtu/compute/se/pisd/roborally/model/board/boardElement/activationElement/Checkpoint.java
+++ src/main/java/dk/dtu/compute/se/pisd/roborally/model/board/boardElement/activationElement/Checkpoint.java
@@ -3,17 +3,30 @@ package dk.dtu.compute.se.pisd.roborally.model.board.boardElement.activationElem
 import dk.dtu.compute.se.pisd.roborally.controller.boardElementController.CheckpointController;
 import dk.dtu.compute.se.pisd.roborally.model.enums.Heading;
 
+
+/**
+ * ...
+ *
+ * @author Tobias Maneschijn, s205422@student.dtu.dk
+ * @author Rasmus Nylander, s205418@student.dtu.dk
+ */
 public class Checkpoint extends ActivationElement {
 
 
-	private int number = 1;
+	/**
+	 * <p>The number of this checkpoint. Reaching
+	 * the checkpoint only counts if a checkpoint
+	 * of the number before has been visited.</p>
+	 */
+	private final int number;
 
 	public int getNumber() {
 		return number;
 	}
 
-	public Checkpoint(Heading[] position, Heading[] impassableFrom, Heading[] opaqueFrom, Heading direction, int priority) {
+	public Checkpoint(Heading[] position, Heading[] impassableFrom, Heading[] opaqueFrom, Heading direction, int priority, int checkpointNumber) {
 		super(position, impassableFrom, opaqueFrom, direction, priority);
+		this.number = checkpointNumber;
 	}
 
 	@Override
